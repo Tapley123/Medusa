fcd35640ee2273537f427a70aa4e8e2d733965e5
Tapley123 <conortapley1@gmail.com>
2020-07-19 08:16:07 -0800

    Player Movement fixed

--- Medusa/Assets/PlayerController.cs
+++ Medusa/Assets/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     float moveDirection = 0;
     bool isGrounded = false;
     Vector3 cameraPos;
-    Rigidbody2D r2d;
+    Rigidbody2D rigidbody2D;
     Collider2D mainCollider;
     // Check every collider except Player and Ignore Raycast
     LayerMask layerMask = ~(1 << 2 | 1 << 8);
@@ -24,11 +24,11 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         t = transform;
-        r2d = GetComponent<Rigidbody2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
         mainCollider = GetComponent<Collider2D>();
-        r2d.freezeRotation = true;
-        r2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-        r2d.gravityScale = gravityScale;
+        rigidbody2D.freezeRotation = true;
+        rigidbody2D.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        rigidbody2D.gravityScale = gravityScale;
         facingRight = t.localScale.x > 0;
         gameObject.layer = 8;
 
@@ -39,14 +39,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Movement controls
-        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && (isGrounded || r2d.velocity.x > 0.01f))
+        // Movement controls                                      **OLD MOVEMENT**
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) //&& (isGrounded || rigidbody2D.velocity.x > 0.01f))
         {
             moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
         }
         else
         {
-            if (isGrounded || r2d.velocity.magnitude < 0.01f)
+            if (isGrounded || rigidbody2D.velocity.magnitude < 0.01f)
             {
                 moveDirection = 0;
             }
@@ -70,7 +70,7 @@ public class PlayerController : MonoBehaviour
         // Jumping
         if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
-            r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpHeight);
         }
 
         // Camera follow
@@ -86,7 +86,7 @@ public class PlayerController : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(groundCheckPos, 0.23f, layerMask);
 
         // Apply movement velocity
-        r2d.velocity = new Vector2((moveDirection) * maxSpeed, r2d.velocity.y);
+        rigidbody2D.velocity = new Vector2((moveDirection) * maxSpeed, rigidbody2D.velocity.y);
 
         // Simple debug
         Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, 0.23f, 0), isGrounded ? Color.green : Color.red);
